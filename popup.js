4e5c1c91f129d567833e5ecd86a40a6b40b19d13
gotens1211 <nikeythe@gmail.com>
2017-04-13 02:38:54 +0530

    Add the input and search through history functionaity

--- popup.js
+++ popup.js
@@ -1,118 +1,118 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+document.getElementById("in1").addEventListener("keyup", srch);
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
+function srch() {
+    var nn = document.getElementById("nice");
+    nn.innerHTML = "";
+    dd = []
+    var key = this.value;
+    console.log(key);
+    for (var i = 0;i<hist.length;i++ ){
+      if (hist[i].includes(key)) {
+        dd.push(hist[i]);
+      }
     }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
+    // nn.innerHTML = dd;
+    buildPopupDom("nice",dd)
 }
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+// Event listner for clicks on links in a browser action popup.
+// Open the link in a new tab of the current window.
+function onAnchorClick(event) {
+    chrome.tabs.create({
+        selected: true,
+        url: event.srcElement.href
     });
-  });
+    return false;
+}
+// Given an array of URLs, build a DOM list of those URLs in the
+// browser action popup.
+function buildPopupDom(divName, data) {
+    var popupDiv = document.getElementById(divName);
+    var ul = document.createElement('ul');
+    popupDiv.appendChild(ul);
+    for (var i = 0, ie = data.length; i < ie; ++i) {
+        var a = document.createElement('a');
+        a.href = data[i];
+        a.appendChild(document.createTextNode(data[i]));
+        a.addEventListener('click', onAnchorClick);
+        var li = document.createElement('li');
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+}
+
+hist = [];
+// Search history to find up to ten links that a user has typed in,
+// and show those links in a popup.
+function buildTypedUrlList(divName) {
+    // To look for history items visited in the last week,
+    // subtract a week of microseconds from the current time.
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+    // Track the number of callbacks from chrome.history.getVisits()
+    // that we expect to get.  When it reaches zero, we have all results.
+    var numRequestsOutstanding = 0;
+    chrome.history.search({
+            'text': '', // Return every history item....
+            'maxResults': 500 // that was accessed less than one week ago.
+        },
+        function(historyItems) {
+            // For each history item, get details on all visits.
+            for (var i = 0; i < historyItems.length; ++i) {
+                var url = historyItems[i].url;
+                hist.push(url);
+                // var processVisitsWithUrl = function(url) {
+                //   // We need the url of the visited item to process the visit.
+                //   // Use a closure to bind the  url into the callback's args.
+                //   return function(visitItems) {
+                //     processVisits(url, visitItems);
+                //   };
+                // };
+                // chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
+                // numRequestsOutstanding++;
+            }
+            // if (!numRequestsOutstanding) {
+            //   onAllVisitsProcessed();
+            // }
+            // buildPopupDom(divName, hist);
+        });
+    // Maps URLs to a count of the number of times the user typed that URL into
+    // the omnibox.
+    // var urlToCount = {};
+    // // Callback for chrome.history.getVisits().  Counts the number of
+    // // times a user visited a URL by typing the address.
+    // var processVisits = function(url, visitItems) {
+    //   for (var i = 0, ie = visitItems.length; i < ie; ++i) {
+    //     // Ignore items unless the user typed the URL.
+    //     if (visitItems[i].transition != 'typed') {
+    //       continue;
+    //     }
+    //     if (!urlToCount[url]) {
+    //       urlToCount[url] = 0;
+    //     }
+    //     urlToCount[url]++;
+    //   }
+    //   // If this is the final outstanding call to processVisits(),
+    //   // then we have the final results.  Use them to build the list
+    //   // of URLs to show in the popup.
+    //   if (!--numRequestsOutstanding) {
+    //     onAllVisitsProcessed();
+    //   }
+    // };
+    // // This function is called when we have the final list of URls to display.
+    // var onAllVisitsProcessed = function() {
+    //   // Get the top scorring urls.
+    //   urlArray = [];
+    //   for (var url in urlToCount) {
+    //     urlArray.push(url);
+    //   }
+    //   // Sort the URLs by the number of times the user typed them.
+    //   urlArray.sort(function(a, b) {
+    //     return urlToCount[b] - urlToCount[a];
+    //   });
+    //   buildPopupDom(divName, urlArray.slice(0, 10));
+    // };
+}
+document.addEventListener('DOMContentLoaded', function() {
+    buildTypedUrlList("typedUrl_div");
 });
